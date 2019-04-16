05dc9ffaecb977214c932a98367de1c62b306a3a
Nikola Velkovski <parabolic@users.noreply.github.com>
2019-04-16 22:46:26 +0200

    Cloudwatch agent (#1)
    * Add documentation
    
    * Add a gitignore and travis yaml
    
    * add a makefile
    
    * add all the variables related to this project
    
    * add the outputs that will be imported
    
    * add the cloud-init configuration for installing and starting the
    cloudwatch agent
    
    * add the standard and advanced cloudwatch configuration
    
    * add the main.tf template for this module
    
    * Update README.yaml
    
    Co-Authored-By: parabolic <parabolic@users.noreply.github.com>
    
    * Update README.yaml
    
    change the namespace so that it doesn't collide with the existing ones.
    
    Co-Authored-By: parabolic <parabolic@users.noreply.github.com>
    
    * Update main.tf
    
    Co-Authored-By: parabolic <parabolic@users.noreply.github.com>
    
    * add all the cloudwatch related project
    
    * Update README.yaml
    
    Typo
    
    Co-Authored-By: parabolic <parabolic@users.noreply.github.com>
    
    * Update README.yaml
    
    typo
    
    Co-Authored-By: parabolic <parabolic@users.noreply.github.com>
    
    * use aws_iam_policy_document instead of HEREDOC
    
    * Update variables.tf
    
    typo
    
    Co-Authored-By: parabolic <parabolic@users.noreply.github.com>
    
    * update the descirption for the disk_resources parameter
    
    * Update variables.tf
    
    typo
    
    Co-Authored-By: parabolic <parabolic@users.noreply.github.com>
    
    * Update variables.tf
    
    typo
    
    Co-Authored-By: parabolic <parabolic@users.noreply.github.com>
    
    * Update variables.tf
    
    typo
    
    Co-Authored-By: parabolic <parabolic@users.noreply.github.com>
    
    * make the namespace required
    
    * Update variables.tf
    
    typo
    
    Co-Authored-By: parabolic <parabolic@users.noreply.github.com>
    
    * fix the variables descriptions and use stage instead of environments
    
    * use stage instead of environment
    
    * update the readme
    
    * add the important text , remove empty git repo
    
    * use usage instead of examples and regenerate the README.md file

---
+++ variables.tf
@@ -0,0 +1,74 @@
+variable "aggregation_dimensions" {
+  description = "Specifies the dimensions that collected metrics are to be aggregated on."
+  type        = "list"
+
+  default = [
+    ["InstanceId"],
+    ["AutoScalingGroupName"],
+  ]
+}
+
+variable "cpu_resources" {
+  description = "Specifies that per-cpu metrics are to be collected. The only allowed value is *. If you include this field and value, per-cpu metrics are collected."
+  type        = "string"
+  default     = "\"resources\": [\"*\"],"
+}
+
+variable "disk_resources" {
+  description = "Specifies an array of disk mount points. This field limits CloudWatch to collect metrics from only the listed mount points. You can specify * as the value to collect metrics from all mount points. Defaults to the root / mountpount."
+  type        = "list"
+  default     = ["/"]
+}
+
+variable "userdata_part_content" {
+  description = "The user data that should be passed along from the caller of the module."
+  type        = "string"
+  default     = ""
+}
+
+variable "userdata_part_content_type" {
+  description = "What format is userdata_part_content in - eg 'text/cloud-config' or 'text/x-shellscript'."
+  type        = "string"
+  default     = "text/cloud-config"
+}
+
+variable "userdata_part_merge_type" {
+  description = "Control how cloud-init merges user-data sections."
+  type        = "string"
+  default     = "list(append)+dict(recurse_array)+str()"
+}
+
+variable "namespace" {
+  description = "Namespace, which could be your organization name or abbreviation, e.g. 'eg' or 'cp'."
+  type        = "string"
+}
+
+variable "stage" {
+  description = "Stage, e.g. 'prod', 'staging', 'dev', or 'test'."
+  type        = "string"
+  default     = ""
+}
+
+variable "name" {
+  description = "Solution name, e.g. 'app'."
+  type        = "string"
+}
+
+variable "metrics_config" {
+  description = <<EOF
+  "Which metrics should we send to cloudwatch, the default is standard. Setting this variable to advanced will send all the available metrics that are provided by the agent.
+  You can find more information here https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/create-cloudwatch-agent-configuration-file-wizard.html."
+EOF
+
+  type    = "string"
+  default = "standard"
+}
+
+variable "metrics_collection_interval" {
+  description = <<EOF
+  Specifies how often to collect the cpu metrics, overriding the global metrics_collection_interval specified in the agent section of the configuration file. If you set this value below 60 seconds, each metric is collected as a high-resolution metric.
+EOF
+
+  type    = "string"
+  default = 60
+}
